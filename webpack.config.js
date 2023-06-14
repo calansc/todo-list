060837e12b02e9519394c104259565f0880b5f09
Chad Schroeder <77646640+calansc@users.noreply.github.com>
2023-06-14 09:53:58 -0400

    Add npm package babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,16 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
     ],
   },
 };
